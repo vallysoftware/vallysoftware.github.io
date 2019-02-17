c9711c5a39fa59b05106261f8c0cf8559c863b93
Robert Kuznetsov <robert@malanka.pro>
2019-02-17 23:54:26 +0300

    Add logic for range.trade button.

--- docs/js/portfolio.js
+++ docs/js/portfolio.js
@@ -1,3 +1,40 @@
 ;(function () {
-  document.addEventListener('')
+  let btnRangeTradeOpen = document.getElementById('btn-range-trade-open');
+  let btnRangeTradeClose = document.getElementById('btn-range-trade-close');
+
+  /**
+   * Adding event listeners
+   */
+  btnRangeTradeOpen.addEventListener('click', clickBtnRangeTrade);
+  btnRangeTradeClose.addEventListener('click', clickBtnRangeTradeClose);
+
+  /**
+   * Event Handler for clict to Range.Trade portfolio button
+   *
+   * @author Robert Kuznetsov
+   */
+  function clickBtnRangeTrade() {
+    let startState = document.getElementById('start-state');
+    let rangeTradeState = document.getElementById('range-trade-state');
+    let body = document.getElementsByTagName('body');
+
+    startState.style.display = 'none';
+    rangeTradeState.style.display = 'block';
+    body[0].setAttribute('id', 'range-trade-portfolio');
+  }
+
+  /**
+   * Event Handler for clict to Range.Trade portfolio close button
+   *
+   * @author Robert Kuznetsov
+   */
+  function clickBtnRangeTradeClose() {
+    let startState = document.getElementById('start-state');
+    let rangeTradeState = document.getElementById('range-trade-state');
+    let body = document.getElementsByTagName('body');
+
+    startState.style.display = 'block';
+    rangeTradeState.style.display = 'none';
+    body[0].removeAttribute('id');
+  }
 }());
