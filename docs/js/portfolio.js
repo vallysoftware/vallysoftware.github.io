cf72c1bf0c759027de71c76f9b89e3d5edfe9a53
Robert Kuznetsov <robot3@xaker.ru>
2019-03-03 23:27:29 +0300

    Added kaelum portfolio button and screen.

--- docs/js/portfolio.js
+++ docs/js/portfolio.js
@@ -1,16 +1,28 @@
 ;(function () {
   let btnRangeTradeOpen = document.getElementById('btn-range-trade-open');
   let btnRangeTradeClose = document.getElementById('btn-range-trade-close');
+  let btnKaelumOpen = document.getElementById('btn-kaelum-open');
+  let btnKaelumClose = document.getElementById('btn-kaelum-close');
 
   /**
    * Adding event listeners
    */
-  btnRangeTradeOpen.addEventListener('click', clickBtnRangeTrade);
-  btnRangeTradeClose.addEventListener('click', clickBtnRangeTradeClose);
   if (window.innerWidth > 800) {
-    btnRangeTradeOpen.addEventListener('mouseover', mouseOverBtnRangeTrade);
-    btnRangeTradeClose.addEventListener('mouseout', mouseOutBtnRangeTrade);
+    btnRangeTradeOpen.addEventListener('mouseenter', mouseOverBtnRangeTrade);
+    btnRangeTradeClose.addEventListener('mouseleave', mouseOutBtnRangeTrade);
+    btnKaelumOpen.addEventListener('mouseenter', mouseOverBtnKaelum);
+    btnKaelumClose.addEventListener('mouseleave', mouseOutBtnKaelum);
+  } else {
+    btnRangeTradeOpen.addEventListener('click', clickBtnRangeTrade);
+    btnRangeTradeClose.addEventListener('click', clickBtnRangeTradeClose);
+    btnKaelumOpen.addEventListener('click', clickBtnKaelum);
+    btnKaelumClose.addEventListener('click', clickBtnKaelum);
   }
+
+  let clickBtnRangeTradeAfterMoment = timeoutDecorator(clickBtnRangeTrade, 100);
+  let clickBtnRangeTradeCloseAfterMoment = timeoutDecorator(clickBtnRangeTradeClose, 300);
+  let clickBtnKaelumAfterMoment = timeoutDecorator(clickBtnKaelum, 100);
+  let clickBtnKaelumCloseAfterMoment = timeoutDecorator(clickBtnKaelumClose, 300);
 
   /**
    * Wrap for clickBtnRangeTrade function
@@ -18,7 +30,7 @@
    * @author Robert Kuznetsov
    */
   function mouseOverBtnRangeTrade(event) {
-    clickBtnRangeTrade();
+    clickBtnRangeTradeAfterMoment();
   }
 
   /**
@@ -27,7 +39,25 @@
    * @author Robert Kuznetsov
    */
   function mouseOutBtnRangeTrade(event) {
-    clickBtnRangeTradeClose();
+    clickBtnRangeTradeCloseAfterMoment();
+  }
+
+  /**
+   * Wrap for clickBtnKaelum function
+   *
+   * @author Robert Kuznetsov
+   */
+  function mouseOverBtnKaelum(event) {
+    clickBtnKaelumAfterMoment();
+  }
+
+  /**
+   * Wrap for clickBtnKaelumClose function
+   *
+   * @author Robert Kuznetsov
+   */
+  function mouseOutBtnKaelum(event) {
+    clickBtnKaelumCloseAfterMoment();
   }
 
   /**
@@ -46,6 +76,24 @@
   }
 
   /**
+   * Event Handler for click to Kaelum portfolio button
+   *
+   * @author Robert Kuznetsov
+   */
+  function clickBtnKaelum() {
+    console.log('clickBtnKaelum')
+    let startState = document.getElementById('start-state');
+    let kaelumState = document.getElementById('kaelum-state');
+    let body = document.getElementsByTagName('body');
+
+    startState.style.display = 'none';
+    startState.style.backgroundImage = 'none';
+    kaelumState.style.display = 'block';
+
+    body[0].setAttribute('id', 'kaelum-portfolio');
+  }
+
+  /**
    * Event Handler for click to Range.Trade portfolio close button
    *
    * @author Robert Kuznetsov
@@ -58,5 +106,37 @@
     startState.style.display = 'block';
     rangeTradeState.style.display = 'none';
     body[0].removeAttribute('id');
+  }
+
+  /**
+   * Event Handler for click to Kaelum portfolio close button
+   *
+   * @author Robert Kuznetsov
+   */
+  function clickBtnKaelumClose() {
+    let startState = document.getElementById('start-state');
+    let kaelumState = document.getElementById('kaelum-state');
+    let body = document.getElementsByTagName('body');
+
+    startState.style.display = 'block';
+    startState.style.backgroundImage = 'url(../images/v.png)';
+    kaelumState.style.display = 'none';
+    body[0].removeAttribute('id');
+  }
+
+  /**
+   * Timeout decorator for setTimeout function
+   *
+   * @author Robert Kuznetsov <robert@malanka.pro>
+   *
+   * @param function which it call with timeout
+   * @param int timeout in ms
+   *
+   * @return function
+   */
+  function timeoutDecorator(func, timeout) {
+      return function () {
+          setTimeout(func, timeout);
+      }
   }
 }());
